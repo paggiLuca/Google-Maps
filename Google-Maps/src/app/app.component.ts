a117126c172ca2e0ff1de370a0fc64cc027463f5
paggiLuca <paggi.luca@iisgalvanimi.edu.it>
2022-05-29 19:11:40 +0000

    fine

--- Google-Maps/src/app/app.component.ts
+++ Google-Maps/src/app/app.component.ts
@@ -8,13 +8,36 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Google-Maps';
 
+
+
   center : any;
   position : any;
   label :string;
+  circleOptions: { fillColor: string; };
+  markerOptions: google.maps.MarkerOptions;
+  vertices: { lat: any; lng: any; }[];
+
+
   constructor()
   {
-    this.center={lat: 45.5248996, lng: 9.1744752};
+    this.center={lat: 45.506738, lng: 9.190766};
     this.position = this.center;
-    this.label = "ciao";
+    this.label = "Scuola";
+
+    this.circleOptions = {fillColor : 'red'}
+    let iconData : google.maps.Icon = {
+      url:'./assets/img/cat_acrobat.ico',
+      scaledSize : new google.maps.Size(60,60)
+    }
+     this.markerOptions = {icon:iconData}
+     
+     this.vertices = [
+      {  lat: this.center.lat + 0.001, lng: this.center.lng - 0.002 },
+      {  lat: this.center.lat, lng: this.center.lng },
+      {  lat: this.center.lat - 0.001, lng: this.center.lng - 0.002},
+ ];
+
+
   }
 }
+
